3d065bba795cf47dc0e753a35173e91f317d2b65
Bartosz Leper <bl.nero@gmail.com>
2019-05-11 23:25:39 +0200

    Make names consistent

--- src/tia.rs
+++ src/tia.rs
@@ -27,17 +27,17 @@ impl TIA {
     pub fn tick(&mut self) -> Output {
         match self.column {
             0 => self.hblank_on = true,
-            H_SYNC_START => self.hsync_on = true,
-            H_SYNC_END => self.hsync_on = false,
-            H_BLANK_WIDTH => self.hblank_on = false,
+            HSYNC_START => self.hsync_on = true,
+            HSYNC_END => self.hsync_on = false,
+            HBLANK_WIDTH => self.hblank_on = false,
             _ => {}
         }
 
         let vsync_on = self.reg_vsync & flags::VSYNC_ON != 0;
         let vblank_on = self.reg_vblank & flags::VBLANK_ON != 0;
         let output = Output {
-            horizontal_sync: self.hsync_on,
-            vertical_sync: vsync_on,
+            hsync: self.hsync_on,
+            vsync: vsync_on,
             pixel: if self.hblank_on || vblank_on {
                 None
             } else {
@@ -66,44 +66,44 @@ impl TIA {
 // We need to derive PartialEq to easily perform assertions in tests.
 #[derive(PartialEq, Clone, Debug)]
 pub struct Output {
-    vertical_sync: bool,
-    horizontal_sync: bool,
+    vsync: bool,
+    hsync: bool,
     pixel: Option<u8>,
 }
 
 impl Output {
     pub fn pixel(pixel: u8) -> Output {
         Output {
-            vertical_sync: false,
-            horizontal_sync: false,
+            vsync: false,
+            hsync: false,
             pixel: Some(pixel),
         }
     }
 
     pub fn blank() -> Output {
         Output {
-            vertical_sync: false,
-            horizontal_sync: false,
+            vsync: false,
+            hsync: false,
             pixel: None,
         }
     }
 
-    pub fn with_horizontal_sync(mut self) -> Self {
-        self.horizontal_sync = true;
+    pub fn with_hsync(mut self) -> Self {
+        self.hsync = true;
         self
     }
 
-    pub fn with_vertical_sync(mut self) -> Self {
-        self.vertical_sync = true;
+    pub fn with_vsync(mut self) -> Self {
+        self.vsync = true;
         self
     }
 }
 
-const H_SYNC_START: i32 = 16;
-const H_SYNC_END: i32 = 32; // 1 cycle after, to make it easy to construct a range.
-const H_BLANK_WIDTH: i32 = 68;
+const HSYNC_START: i32 = 16;
+const HSYNC_END: i32 = 32; // 1 cycle after, to make it easy to construct a range.
+const HBLANK_WIDTH: i32 = 68;
 const FRAME_WIDTH: i32 = 160;
-const TOTAL_WIDTH: i32 = FRAME_WIDTH + H_BLANK_WIDTH;
+const TOTAL_WIDTH: i32 = FRAME_WIDTH + HBLANK_WIDTH;
 
 // const FRAME_HEIGHT: i32 = 192;
 // const VSYNC_HEIGHT: i32 = 3;
@@ -141,7 +141,7 @@ mod tests {
     #[test]
     fn draws_background_pixels() {
         let mut tia = TIA::new();
-        for _ in 0..H_BLANK_WIDTH {
+        for _ in 0..HBLANK_WIDTH {
             tia.tick();
         }
 
@@ -155,9 +155,9 @@ mod tests {
     #[test]
     fn draws_scanlines() {
         let mut expected_output = Vec::new();
-        expected_output.resize(H_SYNC_START as usize, Output::blank());
-        expected_output.resize(H_SYNC_END as usize, Output::blank().with_horizontal_sync());
-        expected_output.resize(H_BLANK_WIDTH as usize, Output::blank());
+        expected_output.resize(HSYNC_START as usize, Output::blank());
+        expected_output.resize(HSYNC_END as usize, Output::blank().with_hsync());
+        expected_output.resize(HBLANK_WIDTH as usize, Output::blank());
         expected_output.resize(TOTAL_WIDTH as usize, Output::pixel(0x80));
         expected_output.append(&mut expected_output.clone());
 
@@ -170,16 +170,13 @@ mod tests {
     #[test]
     fn emits_vsync() {
         let mut expected_output = Vec::new();
-        expected_output.resize(H_SYNC_START as usize, Output::blank().with_vertical_sync());
+        expected_output.resize(HSYNC_START as usize, Output::blank().with_vsync());
         expected_output.resize(
-            H_SYNC_END as usize,
-            Output::blank().with_vertical_sync().with_horizontal_sync(),
+            HSYNC_END as usize,
+            Output::blank().with_vsync().with_hsync(),
         );
-        expected_output.resize(H_BLANK_WIDTH as usize, Output::blank().with_vertical_sync());
-        expected_output.resize(
-            TOTAL_WIDTH as usize,
-            Output::pixel(0x12).with_vertical_sync(),
-        );
+        expected_output.resize(HBLANK_WIDTH as usize, Output::blank().with_vsync());
+        expected_output.resize(TOTAL_WIDTH as usize, Output::pixel(0x12).with_vsync());
 
         let mut tia = TIA::new();
         tia.write(registers::COLUBK, 0x12);
@@ -196,8 +193,8 @@ mod tests {
     #[test]
     fn emits_vblank() {
         let mut expected_output = Vec::new();
-        expected_output.resize(H_SYNC_START as usize, Output::blank());
-        expected_output.resize(H_SYNC_END as usize, Output::blank().with_horizontal_sync());
+        expected_output.resize(HSYNC_START as usize, Output::blank());
+        expected_output.resize(HSYNC_END as usize, Output::blank().with_hsync());
         expected_output.resize(TOTAL_WIDTH as usize, Output::blank());
 
         let mut tia = TIA::new();
@@ -208,7 +205,7 @@ mod tests {
 
         // Make sure that only bit 1 of VBLANK counts.
         tia.write(registers::VBLANK, !flags::VBLANK_ON);
-        for _ in 0..H_BLANK_WIDTH {
+        for _ in 0..HBLANK_WIDTH {
             tia.tick();
         }
         assert_eq!(tia.tick(), Output::pixel(0x32));
@@ -217,12 +214,12 @@ mod tests {
     #[test]
     fn emits_vblank_with_vsync() {
         let mut expected_output = Vec::new();
-        expected_output.resize(H_SYNC_START as usize, Output::blank().with_vertical_sync());
+        expected_output.resize(HSYNC_START as usize, Output::blank().with_vsync());
         expected_output.resize(
-            H_SYNC_END as usize,
-            Output::blank().with_vertical_sync().with_horizontal_sync(),
+            HSYNC_END as usize,
+            Output::blank().with_vsync().with_hsync(),
         );
-        expected_output.resize(TOTAL_WIDTH as usize, Output::blank().with_vertical_sync());
+        expected_output.resize(TOTAL_WIDTH as usize, Output::blank().with_vsync());
 
         let mut tia = TIA::new();
         tia.write(registers::VSYNC, flags::VSYNC_ON);
