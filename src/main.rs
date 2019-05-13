afbb83165c7bd90d4f5c0ed6ae8877794928d37c
Bartosz Leper <bl.nero@gmail.com>
2019-05-13 20:17:37 +0200

    Implement basic TIA features (#2)
    * Implement horizontal sync
    
    * Generate vsync
    
    * Simplify the tests
    
    * Simplify TIA::tick()
    
    * Implement vertical blanking
    
    * Make names consistent
    
    * Cleanup, comments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 pub mod cpu;
 pub mod memory;
+pub mod tia;
 
 
 fn main() {
     use cpu::CPU;
     use memory::RAM;
+    use tia::TIA;
 
     println!("Welcome player ONE!");
     let mut memory = RAM::new(&[]);
     let cpu = CPU::new(&mut memory);
     println!("{:#?}", cpu);
+
+    let tia = TIA::new();
 }
