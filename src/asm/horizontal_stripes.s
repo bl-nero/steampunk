422a05cb2bbdb46dcf2a6bb4c9ee1f1941c39439
Bartosz Leper <bl.nero@gmail.com>
2019-05-26 22:17:06 +0200

    Add a build script
    The build script uses the ca65 assembler (not included, part of the cc65
    compiler suite) to assemble sample ROMs for testing. A simple kernel
    file is included.

---
+++ src/asm/horizontal_stripes.s
@@ -0,0 +1,55 @@
+; This file is based on Andrew Davie's tutorial
+; (https://www.randomterrain.com/atari-2600-memories-tutorial-andrew-davie-08.html).
+
+.include "atari2600.inc"
+
+Reset:
+StartOfFrame:
+            ; Start vertical blanking.
+            lda #%01000010
+            sta VBLANK
+
+            ; Emit 3 scanlines of VSYMC.
+            lda #2
+            sta VSYNC
+            sta WSYNC
+            sta WSYNC
+            sta WSYNC
+
+            ; Emit 37 scanlines of vertical blank.
+            lda #0
+            sta VSYNC           
+            .repeat 37
+                sta WSYNC
+            .endrepeat
+
+            ; Emit 192 scanlines of picture. Increment color with each line so
+            ; that each line gets a different color, until we run out of them
+            ; and start over. Note that we increment the color twice, since
+            ; only even color numbers are actually distinguishable.
+            lda #0
+            sta VBLANK
+            ldx #0
+            .repeat 192
+                stx COLUBK
+                sta WSYNC
+                inx
+                inx
+            .endrepeat
+ 
+            ; Start vertical blanking.
+            lda #%01000010
+            sta VBLANK
+
+            ; Emit 30 scanlines of overscan.
+            .repeat 30
+                sta WSYNC
+            .endrepeat
+
+            jmp StartOfFrame
+
+.segment "VECTORS"
+
+            .word Reset          ; NMI
+            .word Reset          ; RESET
+            .word Reset          ; IRQ
